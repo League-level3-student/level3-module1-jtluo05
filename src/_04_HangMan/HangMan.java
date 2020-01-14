b761f0eda4eea7ce6d4bc93a8545e6413d84378c
League Student <league@iMac-24.attlocal.net>
2020-01-13 19:55:55 -0800

    hangman ip
    :ski:

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -15,27 +15,28 @@ public class HangMan implements KeyListener{
 	static String word = "";
 	static JFrame frame = new JFrame();
 	static JLabel label = new JLabel();
-
+     static String p; 
+     String underscore="";
+      int lettercount=0;
 	public static void main(String[] args) {
 	HangMan hangman=new HangMan();
-		hangman.stuff();
-		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+			
 		String input = JOptionPane.showInputDialog("How many words do you want to solve?");
 		int numOfWords = Integer.parseInt(input);
 		for (int i = 0; i < numOfWords; i++) {
 			word = Utilities.readRandomLineFromFile("dictionary.txt");
 			hm.push(word);
+				
 				}
-		
+		for (int i = 0; i < numOfWords; i++) {
+			p=hm.pop();
+				}
+	hangman.lettercount=p.length();
+			hangman.stuff();
+	
+	
 	}
 	
-	 void stuff() {
-		frame.setVisible(true);
-		frame.add(label);	
-		label.setText("");
-frame.addKeyListener(this);
-		frame.setSize(300, 300);
-	}
 
 	 
 	@Override
@@ -47,7 +48,18 @@ frame.addKeyListener(this);
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+String ontolabel;
+		for (int i = 0; i < lettercount; i++) {
+		ontolabel=underscore.charAt(i)+"";
+	if (e.getKeyChar()==p.charAt(i)) {
 		
+		
+		ontolabel=ontolabel.substring(0, i)+p.charAt(i)+ ontolabel.substring(i);
+		
+		
+		label.setText(ontolabel);
+}	
+}
 	}
 
 	@Override
@@ -55,5 +67,16 @@ frame.addKeyListener(this);
 		// TODO Auto-generated method stub
 		
 	}
-
+void stuff() {
+		frame.setVisible(true);
+		frame.add(label);
+		for (int i = 0; i < lettercount; i++) {
+			underscore=underscore+"_";
+		
+		}
+		label.setText(underscore);
+frame.addKeyListener(this);
+		frame.setSize(300, 300);
+			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	}
 }
