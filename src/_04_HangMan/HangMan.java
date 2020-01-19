e00f999e54976bef4f1a9ea10e53ea6ade5e2496
League Student <league@iMac-9.attlocal.net>
2020-01-19 12:55:56 -0800

    hangman ip

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,5 +1,6 @@
 package _04_HangMan;
 
+import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
@@ -8,37 +9,37 @@ import java.util.Stack;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class HangMan implements KeyListener{
+public class HangMan implements KeyListener {
 	Random r = new Random();
 	static Stack<String> hm = new Stack<String>();
 	static String word = "";
 	static JFrame frame = new JFrame();
 	static JLabel label = new JLabel();
-     static String p; 
-     String underscore="";
-      int lettercount=0;
+	static JPanel panel=new JPanel();
+	static String p;
+	String underscore = "";
+	int lettercount = 0;
+
 	public static void main(String[] args) {
-	HangMan hangman=new HangMan();
-			
+		HangMan hangman = new HangMan();
+
 		String input = JOptionPane.showInputDialog("How many words do you want to solve?");
 		int numOfWords = Integer.parseInt(input);
 		for (int i = 0; i < numOfWords; i++) {
 			word = Utilities.readRandomLineFromFile("dictionary.txt");
 			hm.push(word);
-				
-				}
-		for (int i = 0; i < numOfWords; i++) {
-			p=hm.pop();
-				}
-	hangman.lettercount=p.length();
-			hangman.stuff();
-	
-	
-	}
-	
 
-	 
+		}
+		for (int i = 0; i < numOfWords; i++) {
+			p = hm.pop();
+		}
+		hangman.lettercount = p.length();
+		hangman.stuff();
+
+	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -48,35 +49,48 @@ public class HangMan implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-String ontolabel;
+		
+		System.out.println(lettercount);
+		System.out.println(p);
+
 		for (int i = 0; i < lettercount; i++) {
-		ontolabel=underscore.charAt(i)+"";
-	if (e.getKeyChar()==p.charAt(i)) {
-		
-		
-		ontolabel=ontolabel.substring(0, i)+p.charAt(i)+ ontolabel.substring(i);
-		
-		
-		label.setText(ontolabel);
-}	
-}
+			
+			if (e.getKeyChar() == p.charAt(i)) {
+				if (i == 0) {
+
+					underscore = p.charAt(i) + underscore.substring(i);
+
+					
+				}
+				if (i == lettercount - 1) {
+					underscore = underscore.substring(0, lettercount - 1) + p.charAt(i);
+				} 
+				if(i>0&&i<lettercount-1){
+					underscore=underscore.substring(0, i)+p.charAt(i)+ underscore.substring(i);
+				}
+			}
+			label.setText(underscore);
+frame.pack();
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-void stuff() {
+
+	void stuff() {
 		frame.setVisible(true);
-		frame.add(label);
+		panel.add(label);
 		for (int i = 0; i < lettercount; i++) {
-			underscore=underscore+"_";
-		
+			underscore = underscore + "_";
+
 		}
 		label.setText(underscore);
-frame.addKeyListener(this);
-		frame.setSize(300, 300);
-			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.addKeyListener(this);
+		frame.add(panel);
+		panel.setPreferredSize(new Dimension(300,300));
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 	}
 }
